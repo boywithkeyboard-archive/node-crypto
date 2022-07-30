786ca5a1761e70f9672748dd55ca21766a3c3443
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2022-07-31 01:37:04 +0200

    chore: update decrypt.ts

--- decrypt.ts
+++ decrypt.ts
@@ -2,14 +2,12 @@ import { createDecipheriv } from 'node:crypto'
 
 const encryptionKey = '' // 32-byte string
 
-, decrypt = async (text: any) => {
+export const decrypt = async (text: string) => {
   const textArray = text.split(':')
-  , iv = Buffer.from(textArray.shift(), 'hex')
+  , iv = Buffer.from(textArray.shift() as string, 'hex')
   , encryptedText = Buffer.from(textArray.join(':'), 'hex')
   , decipher = createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv)
   , decryptedText = Buffer.concat([decipher.update(encryptedText), decipher.final()])
  
   return decryptedText.toString()
 }
-
-export default decrypt
